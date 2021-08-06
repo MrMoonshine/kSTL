028b01a568a698829f11430566c542eff185d25c
MrMoonshine <mrmoonshine.goo@gmail.com>
2021-08-06 12:44:29 +0200

    colourful cube

--- src/glwrap.c
+++ src/glwrap.c
@@ -11,7 +11,9 @@
 
 static GtkWidget* glarea;
 static const char* TAG = "OpenGL";
-static GLuint vertexbuffer;
+
+static GLuint vertexbuffer, colourbuffer;
+
 static GLuint shaderID;
 static GLuint mvpID;
 static GLuint vertexArrayID;
@@ -27,7 +29,6 @@ static const float movement_mouse_speed = 3.0f;
 static float movement_angle_horizontal = 0.0f;
 static float movement_angle_vertical = 0.0f;
 static vec2 *movement_offset_buffer;
-static vec3 movement_camera_position;
 // Hold init data for GTK signals:
 struct signal {
 	const gchar	*signal;
@@ -49,6 +50,95 @@ static void dump_mat4(float *mat, const char* title){
 	printf("O----------------------O\n");
 }
 
+static void init_test_cube_buffers(){
+	//Cube data from: https://github.com/opengl-tutorials/ogl/blob/master/tutorial04_colored_cube/tutorial04.cpp
+	const GLfloat test_cube_vertex_buffer_data[] = { 
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f,-1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-1.0f,-1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f,-1.0f,
+		 1.0f,-1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f,-1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f,-1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f, 1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		 1.0f,-1.0f, 1.0f
+	};
+
+	const GLfloat test_cube_color_buffer_data[] = { 
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
+
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(test_cube_vertex_buffer_data), test_cube_vertex_buffer_data, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &colourbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colourbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(test_cube_color_buffer_data), test_cube_color_buffer_data, GL_STATIC_DRAW);
+}
+
 static void handle_perspective(mat4 transform, clock_t delta_t){
 	GLfloat deltaX, deltaY;
 	//gmouse_get_movement_offset(&deltaX, &deltaY);
@@ -62,7 +152,7 @@ static void handle_perspective(mat4 transform, clock_t delta_t){
 
 	vec3 eye = {4,3,-3};
 	vec3 direction = {0,0,0};
-	vec3 up = {0,-1,0};
+	vec3 up = {0,1,0};
 
 	mat4 identity, view, projection, buffer;
 	glm_mat4_identity(identity);
@@ -70,7 +160,7 @@ static void handle_perspective(mat4 transform, clock_t delta_t){
 	glm_perspective(M_PI/4, window_ratio, 0.1f, 100.0f, projection);
 	glm_mat4_mul(projection, view, buffer);
 	glm_mat4_mul(buffer, identity, transform);
-	dump_mat4((float*)transform, "Transform");
+	//dump_mat4((float*)transform, "Transform");
 }
 
 static void on_realize(GtkGLArea *glarea){
@@ -104,18 +194,11 @@ static void on_realize(GtkGLArea *glarea){
 
     shaderID = glshader_load( "../shaders/vertexshd1.glsl", "../shaders/fragmentshd1.glsl" );
 	mvpID = glGetUniformLocation(shaderID, "transform");
+	/*---------------------------------*/
+    /*       Initialize Buffers        */
+    /*---------------------------------*/
+	init_test_cube_buffers();
 
-    static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
-	};
-
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-	glm_vec3_zero(movement_camera_position);
 	previousRenderTime = clock();
 }
 
@@ -137,7 +220,9 @@ static bool on_render(){
     glUseProgram(shaderID);
 
 	glUniformMatrix4fv(mvpID, 1, GL_FALSE, &transformation_matrix[0][0]);
-    // 1rst attribute buffer : vertices
+    /*---------------------------------*/
+    /*       Render Vertices           */
+    /*---------------------------------*/
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glVertexAttribPointer(
@@ -149,10 +234,24 @@ static bool on_render(){
 		(void*)0            // array buffer offset
 	);
 
-	// Draw the triangle !
-	glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+	/*---------------------------------*/
+    /*      Render Colours             */
+    /*---------------------------------*/
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, colourbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			3,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+	// Draw the triangles!
+	glDrawArrays(GL_TRIANGLES, 0, 12*3); // 3 indices starting at 0 -> 1 triangle
 
 	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
     return true;
 }
 
@@ -202,6 +301,7 @@ int glwrap_init_gl(GtkWidget *window, GtkWidget *parentLayout){
 int glwrap_cleanup(){
 	free(movement_offset_buffer);
     glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &colourbuffer);
     glDeleteVertexArrays(1, &vertexArrayID);
     glDeleteProgram(shaderID);
 }
