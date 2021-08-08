e5c13c812cb86bcfc4a5893a60c4b7bfa08ad240
MrMoonshine <mrmoonshine.goo@gmail.com>
2021-08-08 13:16:42 +0200

    convenient callback management

--- src/glwrap.c
+++ src/glwrap.c
@@ -10,7 +10,7 @@
 
 
 static GtkWidget* glarea;
-static const char* TAG = "OpenGL";
+static const char* TAG = "GTK GL";
 
 static GLuint vertexbuffer, colourbuffer;
 
@@ -29,6 +29,8 @@ static const float movement_mouse_speed = 3.0f;
 static float movement_angle_horizontal = 0.0f;
 static float movement_angle_vertical = 0.0f;
 static vec2 *movement_offset_buffer;
+
+
 // Hold init data for GTK signals:
 struct signal {
 	const gchar	*signal;
@@ -139,7 +141,7 @@ static void init_test_cube_buffers(){
 	glBufferData(GL_ARRAY_BUFFER, sizeof(test_cube_color_buffer_data), test_cube_color_buffer_data, GL_STATIC_DRAW);
 }
 
-static void handle_perspective(mat4 transform, clock_t delta_t){
+static void handle_perspective(mat4 transform){
 	GLfloat deltaX, deltaY;
 	//gmouse_get_movement_offset(&deltaX, &deltaY);
 	deltaX = *movement_offset_buffer[0];
@@ -199,22 +201,27 @@ static void on_realize(GtkGLArea *glarea){
     /*---------------------------------*/
 	init_test_cube_buffers();
 
-	previousRenderTime = clock();
+	/*---------------------------------*/
+    /*       Init GTK stuff            */
+    /*---------------------------------*/
+	GdkGLContext *glcontext = gtk_gl_area_get_context(glarea);
+	GdkWindow *glwindow = gdk_gl_context_get_window(glcontext);
+	GdkFrameClock *frame_clock = gdk_window_get_frame_clock(glwindow);
+
+	g_signal_connect_swapped(
+		frame_clock,
+		"update",
+		G_CALLBACK(gtk_gl_area_queue_render),
+		glarea
+	);
+
+	// Start updating:
+	gdk_frame_clock_begin_updating(frame_clock);
 }
 
 static bool on_render(){
-	/*---------------------------------*/
-    /*       Handle Render Times       */
-    /*---------------------------------*/
-	clock_t current_t, delta_t;
-	current_t = clock();
-	delta_t = current_t - previousRenderTime;
-	previousRenderTime = current_t;
-	//Get time unit in ms
-	delta_t = (double)(delta_t * 1000.0f)/ CLOCKS_PER_SEC;
-
 	mat4 transformation_matrix;
-	handle_perspective(transformation_matrix, delta_t);
+	handle_perspective(transformation_matrix);
     // Clear canvas:
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glUseProgram(shaderID);
@@ -262,11 +269,77 @@ static void on_resize(GtkGLArea *area, int width, int height){
 	//printf("[on resize] Offset buffer was: %f %f\n", (*movement_offset_buffer)[0], (*movement_offset_buffer)[1]);
 }
 
+static bool on_button_press (GtkWidget *widget, GdkEventButton *event){
+	//GtkAllocation allocation;
+	//gtk_widget_get_allocation(widget, &allocation);
+
+	if (event->button == 1)
+	printf("Button Press\nprev render time: %d\n",previousRenderTime);
+		/*if (panning == FALSE) {
+			panning = TRUE;
+			model_pan_start(event->x, allocation.height - event->y);
+		}*/
+
+	return FALSE;
+}
+
+static bool on_button_release (GtkWidget *widget, GdkEventButton *event){
+	if (event->button == 1)
+	printf("Button Release\n");
+	//panning = FALSE;
+
+	return FALSE;
+}
+
+static bool on_motion_notify (GtkWidget *widget, GdkEventMotion *event){
+	GtkAllocation allocation;
+	gtk_widget_get_allocation(widget, &allocation);
+
+	//if (panning == TRUE)
+	//model_pan_move(event->x, allocation.height - event->y);
+	printf("Motion Notify");
+
+	return FALSE;
+}
+
+static bool on_scroll (GtkWidget* widget, GdkEventScroll *event){
+	switch (event->direction)
+	{
+	case GDK_SCROLL_UP:
+		printf("Scroll Up");
+		break;
+
+	case GDK_SCROLL_DOWN:
+		printf("Scroll Down");
+		break;
+
+	default:
+		break;
+	}
+
+	return FALSE;
+}
+
 static void connect_signals (GtkWidget *widget, struct signal *signals, size_t members){
 	for(int a = 0; a < members; a++){
         gtk_widget_add_events(widget, signals[a].mask);
         g_signal_connect(widget, signals[a].signal, signals[a].handler, NULL);
     }
+}
+
+static void connect_glarea_signals(GtkWidget *glarea){
+    struct signal signals[] = {
+		{ "realize",			G_CALLBACK(on_realize),		0			},
+		{ "render",			G_CALLBACK(on_render),		0			},
+		{ "resize",			G_CALLBACK(on_resize),		0			},
+		{ "scroll-event",		G_CALLBACK(on_scroll),		GDK_SCROLL_MASK		},
+		{ "button-press-event",		G_CALLBACK(on_button_press),	GDK_BUTTON_PRESS_MASK	},
+		{ "button-release-event",	G_CALLBACK(on_button_release),	GDK_BUTTON_RELEASE_MASK	},
+		{ "motion-notify-event",	G_CALLBACK(on_motion_notify),	GDK_BUTTON1_MOTION_MASK	},
+	};
+	const size_t callbackCount = sizeof(signals)/sizeof(struct signal);
+	//printf("Sizeof signals is: %d\n",callbackCount);
+    connect_signals(glarea, signals, callbackCount);
 }
 
 int glwrap_init_gl(GtkWidget *window, GtkWidget *parentLayout){
@@ -275,22 +348,12 @@ int glwrap_init_gl(GtkWidget *window, GtkWidget *parentLayout){
     /*---------------------------------*/
     glarea = gtk_gl_area_new();
     gtk_box_pack_start(GTK_BOX(parentLayout), glarea, TRUE, TRUE, 0);
-
-    const size_t callbackCount = 3;
-    struct signal signals[] = {
-		{ "realize",			G_CALLBACK(on_realize),		0			},
-		{ "render",			G_CALLBACK(on_render),		0			},
-		{ "resize",			G_CALLBACK(on_resize),		0			},
-		//{ "button-press-event",		G_CALLBACK(glwrap_mouse_click),	GDK_BUTTON_PRESS_MASK	},
-		/*{ "button-release-event",	G_CALLBACK(on_button_release),	GDK_BUTTON_RELEASE_MASK	},
-		{ "motion-notify-event",	G_CALLBACK(on_motion_notify),	GDK_BUTTON1_MOTION_MASK	},*/
-	};
-    connect_signals(glarea, signals, callbackCount);
+    connect_glarea_signals(glarea);
 
 	movement_offset_buffer = (vec2*)malloc(sizeof(*movement_offset_buffer));
 	*movement_offset_buffer[0] = 0;
 	*movement_offset_buffer[1] = 0;
-	gmouse_init_mouse_events(window, movement_offset_buffer);
+	//gmouse_init_mouse_events(window, movement_offset_buffer);
     /*---------------------------------*/
     /*           OpenGL                */
     /*---------------------------------*/
